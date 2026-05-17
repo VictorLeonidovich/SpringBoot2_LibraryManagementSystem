41cb91ddb37aa73f79a0e8a66b87fc673a8ad843
Krasikov_V <VKrasikov@mdait.by>
2026-05-18 00:13:15 +0300

    Set up the Global exception handler

--- src/main/java/com/kvl/library/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kvl/library/exception/GlobalExceptionHandler.java
@@ -1,16 +1,43 @@
 package com.kvl.library.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public String handleException(Exception ex) {
-        log.error("An unhandled exception occurred: ", ex);
-        return "error";
+    @Value("${app.errors.show-details:false}")
+    private boolean showDetails;
+
+    /*@ExceptionHandler(value = {MyException.class})
+    public String handleRecordNotFound(Exception ex, Model model) {
+        log.error("MyException occurred: ", ex);
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error/404";
+    }*/
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String handleNotFoundError(Model model) {
+        log.error("The page not found.");
+        model.addAttribute("errorMessage", "Запрашиваемая страница не найдена.");
+        return "error/404";
     }
+
+    @ExceptionHandler(Exception.class)
+    public String handleGlobalException(Exception ex, Model model) {
+        log.error("An unhandled exception occurred: ", ex);
+        if (showDetails) {
+            model.addAttribute("errorMessage", "Произошла внутренняя ошибка сервера. Пожалуйста, попробуйте позже.");
+            model.addAttribute("technicalDetails", ex.getMessage()); // Не рекомендуется показывать пользователям в продакшене
+        } else {
+            model.addAttribute("technicalDetails", null);
+        }
+        return "error/500.html";
+    }
+
 }
